383455d0767257e41f6304c9bc91931243ae6648
svoma <vomasrikanth5209@gmail.com>
2023-11-30 00:08:45 +0530

    Added contact us

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,12 +6,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about-us', component: AboutUsComponent },
     { path: 'services', component: ServicesComponent },
+    { path: 'contact-us', component: ContactUsComponent }
 ];
 
 @NgModule({
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Rellax from 'rellax';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,7 +10,20 @@ export class ContactUsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    var rellaxHeader = new Rellax('.rellax-header');
+
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('profile-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+  
+  ngOnDestroy() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('profile-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
   }
 
 }
